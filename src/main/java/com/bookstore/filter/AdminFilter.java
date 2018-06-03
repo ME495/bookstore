a760f732f7cc454d179fc81915a56d507e844a4b
ME495 <ME495@qq.com>
2018-06-03 14:18:47 +0800

    完成用户过滤器和管理员过滤器

---
+++ src/main/java/com/bookstore/filter/AdminFilter.java
@@ -0,0 +1,56 @@
+package com.bookstore.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class AdminFilter
+ */
+@WebFilter("/admin/*")
+public class AdminFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public AdminFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		HttpSession httpSession = httpServletRequest.getSession();
+		String role = (String) httpSession.getAttribute("role");
+		if (role == null || !role.equals("admin")) {
+			httpServletResponse.sendRedirect("../admin_login.html");
+		}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/java/com/bookstore/filter/UserFilter.java
@@ -0,0 +1,65 @@
+package com.bookstore.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bookstore.entity.ErrorMessage;
+import com.bookstore.message.ResponseMes;
+import com.bookstore.service.LoginService;
+
+/**
+ * Servlet Filter implementation class UserLoginFilter
+ */
+@WebFilter("/user/*")
+public class UserFilter implements Filter {
+
+	@Autowired
+	private LoginService loginService;
+	
+    /**
+     * Default constructor. 
+     */
+    public UserFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		HttpSession httpSession = httpServletRequest.getSession();
+		String role = (String) httpSession.getAttribute("role");
+		if (role == null || !role.equals("user")) {
+			httpServletResponse.sendRedirect("../user_login.html");
+		}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
