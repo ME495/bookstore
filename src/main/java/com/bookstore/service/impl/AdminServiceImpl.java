162b5f4b1bf0fc6d7b14966d5dbd82490cc82ad9
hzx <3261320184@qq.com>
2018-06-03 16:33:01 +0800

    更换AdminService编写方式

--- src/main/java/com/bookstore/service/impl/AdminServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/AdminServiceImpl.java
@@ -1,6 +1,4 @@
-package com.bookstore.service;
-
-import java.util.regex.Pattern;
+package com.bookstore.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,9 +6,10 @@ import org.springframework.stereotype.Service;
 import com.bookstore.entity.Admin;
 import com.bookstore.mapper.AdminMapper;
 import com.bookstore.message.ResponseMes;
+import com.bookstore.service.AdminService;
 
 @Service
-public class AdminService {
+public class AdminServiceImpl implements AdminService {
 	@Autowired
 	private AdminMapper adminMapper;
 	@Autowired
@@ -18,14 +17,14 @@ public class AdminService {
 
 	public ResponseMes insertAdmin(String adminName, String password) {
 		ResponseMes responseMes;
-		//账号4~16位,以字母开头,包含字母,数字,下划线,密码6~16位包含字母,数字,下划线
+		// 账号4~16位,以字母开头,包含字母,数字,下划线,密码6~16位包含字母,数字,下划线
 		// 判断账号，密码格式是否正确
 		if (adminName.matches("[a-zA-Z][a-zA-Z0-9_]{3,15}") && password.matches("[a-zA-Z0-9_]{6,16}")) {
 			admin.setAdminName(adminName);
 			admin.setPassword(password);
 			if (adminMapper.getAdminByName(adminName) == null) { // 若账户不存在，则添加,否则，返回错误信息
 				adminMapper.insertAdmin(admin);
-				responseMes = new ResponseMes("success","创建成功");
+				responseMes = new ResponseMes("success", "创建成功");
 			} else {
 				responseMes = new ResponseMes("fail", "账号已存在");
 			}
--- src/test/java/com/bookstore/service/test/AdminServiceTest.java
+++ src/test/java/com/bookstore/service/test/AdminServiceTest.java
@@ -8,13 +8,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
 import com.bookstore.message.ResponseMes;
-import com.bookstore.service.AdminService;
+import com.bookstore.service.impl.AdminServiceImpl;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring-cfg.xml" })
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
 public class AdminServiceTest {
 	@Autowired
-	private AdminService adminService;
+	private AdminServiceImpl adminService;
 	@Test
 	public void testInsertAdmin() {
 		ResponseMes responseMes = adminService.insertAdmin("hzx","123456");
