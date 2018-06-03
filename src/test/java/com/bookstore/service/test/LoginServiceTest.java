d09dbbffa3c2b220116f45b794f2070a93cb95fe
ME495 <ME495@qq.com>
2018-06-03 10:29:46 +0800

    完善包结构

---
+++ src/test/java/com/bookstore/service/test/LoginServiceTest.java
@@ -0,0 +1,31 @@
+package com.bookstore.service.test;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.bookstore.service.LoginService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+	"classpath:spring-cfg.xml", 
+	"classpath:mybatis-cfg.xml", 
+	"classpath:dispatcher-servlet.xml"
+})
+public class LoginServiceTest {
+
+	@Autowired
+	private LoginService loginService;
+	
+	@Test
+	public void userLoginTest() {
+		String userName = "chengjian";
+		String password = DigestUtils.md5Hex("123456");
+		assertEquals(loginService.userCheck(userName, password), true);
+	}
+}
