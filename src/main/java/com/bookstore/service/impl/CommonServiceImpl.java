2350d2d9b160ea819b547b30b88fa4cf912a3524
hzx <3261320184@qq.com>
2018-06-30 17:00:46 +0800

    修正图书加载的问题

--- src/main/java/com/bookstore/service/impl/CommonServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/CommonServiceImpl.java
@@ -21,8 +21,12 @@ public class CommonServiceImpl implements CommonService {
 	@Autowired
 	private UserMapper userMapper;
 
-	/* (non-Javadoc)
-	 * @see com.bookstore.service.CommonService#getBookByIsbnAndDegree(java.lang.String, int)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.bookstore.service.CommonService#getBookByIsbnAndDegree(java.lang.String,
+	 * int)
 	 */
 	@Override
 	public ResponseMes getBookByIsbnAndDegree(String isbn, int degree) {
@@ -61,50 +65,45 @@ public class CommonServiceImpl implements CommonService {
 	 */
 	@Override
 	public ResponseMes getBooks(int index, int size) {
-		int count = commonMapper.getBookCount();
-		if (index > count) {//如果开始位置大于数据库的记录数,则翻到最后一页
-			index = count - size;
-		}
 		ArrayList<Book> books = commonMapper.getBooks(index, size);
 		ResponseMes responseMes = new ResponseMes(ResponseMes.SUCCESS, books);
 		return responseMes;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.bookstore.service.CommonService#getBooksByKeyWord(java.lang.String, int, int)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.bookstore.service.CommonService#getBooksByKeyWord(java.lang.String,
+	 * int, int)
 	 */
 	@Override
 	public ResponseMes getBooksByKeyWord(String keyWord, int index, int size) {
-		int count = commonMapper.getBookCount();
-		if (index > count) {//如果开始位置大于数据库的记录数,则翻到最后一页
-			index = count - size;
-		}
 		ArrayList<Book> books = commonMapper.getBooksByKeyWord(keyWord, index, size);
 		ResponseMes responseMes = new ResponseMes(ResponseMes.SUCCESS, books);
 		return responseMes;
 	}
-	
+
 	@Override
-    public ResponseMes insertUser(String userName, String password, String phone, String realName, String address) {
-        ResponseMes responseMes;
-        // 如果手机格式不正确,则返回"手机号码格式错误"
-        if (userName.matches("[a-zA-Z][a-zA-Z0-9_]{3,15}") && password.matches("[a-zA-Z0-9_]{6,16}")
-                && phone.matches("^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$")) {
-            if (userMapper.getUser(userName) == null) {// 如果账号已存在,则返回"该账号已存在"
-                User user = new User();
-                user.setUserName(userName);
-                user.setPassword(password);
-                user.setPhone(phone);
-                user.setRealName(realName);
-                user.setAddress(address);
-                commonMapper.insertUser(user);
-                responseMes = new ResponseMes(ResponseMes.SUCCESS, "注册成功");
-            } else {
-                responseMes = new ResponseMes(ResponseMes.FAIL, "该账号已存在");
-            }
-        } else {
-            responseMes = new ResponseMes(ResponseMes.FAIL, "格式错误");
-        }
-        return responseMes;
-    }
+	public ResponseMes insertUser(String userName, String password, String phone, String realName, String address) {
+		ResponseMes responseMes;
+		// 如果手机格式不正确,则返回"手机号码格式错误"
+		if (userName.matches("[a-zA-Z][a-zA-Z0-9_]{3,15}") && password.matches("[a-zA-Z0-9_]{6,16}")
+				&& phone.matches("^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$")) {
+			if (userMapper.getUser(userName) == null) {// 如果账号已存在,则返回"该账号已存在"
+				User user = new User();
+				user.setUserName(userName);
+				user.setPassword(password);
+				user.setPhone(phone);
+				user.setRealName(realName);
+				user.setAddress(address);
+				commonMapper.insertUser(user);
+				responseMes = new ResponseMes(ResponseMes.SUCCESS, "注册成功");
+			} else {
+				responseMes = new ResponseMes(ResponseMes.FAIL, "该账号已存在");
+			}
+		} else {
+			responseMes = new ResponseMes(ResponseMes.FAIL, "格式错误");
+		}
+		return responseMes;
+	}
 }
